c20d6862e184493b58a6be0e17f3d94d4da5dc09
jlapuente <javierlapuentegarcia@gmail.com>
2023-02-01 14:08:32 +0100

    Flujo entre menus y parametrizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,30 +20,52 @@ export class AppComponent implements OnInit {
         console.log(e);
         console.log(e.keyCode);
         this.isOpen = !this.isOpen;
+        console.log("llamo changeMenu ")
         this.changeMenu();
       }
     });
 
-
-
-
   }
 
+  changeId(id: number) {
+    this.selectedChar = chars[id];
+  }
 
   changeMenu() {
     const element = document.querySelector('.container');
     if (element != null) {
       element.classList.add('animate__animated', 'animate__backOutUp');
-      element.addEventListener('animationend', () => {
+      element.addEventListener('animationend', function change() {
         element.classList.add('hidden');
         console.log("animacion acabada")
         const charDescription = document.querySelector('.char-description');
-        if(charDescription != null){
+        if (charDescription != null) {
           charDescription.classList.remove('hidden');
+          charDescription.classList.remove('animate__animated', 'animate__backOutUp');
         }
       });
     }
-
   }
 
+  back() {
+    this.isOpen = !this.isOpen;
+    this.selectedChar = chars[0];
+    const charDescription = document.querySelector('.char-description');
+    if (charDescription != null) {
+      charDescription.classList.add('animate__animated', 'animate__backOutUp');
+      charDescription.addEventListener('animationend', function changeBack() {
+        charDescription.classList.add('hidden');
+        const element = document.querySelector('.container');
+        if (element != null) {
+          element.classList.remove('hidden');
+          element.classList.remove('animate__animated', 'animate__backOutUp');
+          console.log("animacion acabada");
+        }
+      })
+    }
+  }
+
+  changeBack = function () {
+
+  }
 }
